52bdcf0c9641fdfc262457d33d42f6a5e8d12283
Hiya-Jayaswal <hiya.jayaswal@gmail.com>
2025-04-29 21:36:59 +0530

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import yfinance as yf
+from tabulate import tabulate
+pd.set_option('display.max_columns', None)
+
+stock_symbols = ['AAPL', 'GOOGL', 'AMZN', 'MSFT', 'TSLA', 'META', 'NFLX', 'NVDA', 'SPY', 'BA']
+
+
+stock_data = []
+for symbol in stock_symbols:
+    try:
+        
+        stock = yf.Ticker(symbol)
+        
+        # DATA FOR THE MOST RECENT DAY
+        historical_data = stock.history(period="3d")
+        
+        # CHECK IF DATA EXIST
+        if historical_data.empty:
+            print(f"No data found for {symbol}. Skipping.")
+            continue
+        
+        # GET LATEST PRICE
+        price = historical_data['Close'].iloc[-1]
+        
+        # CLOSING
+        if len(historical_data) > 1:
+            prev_close = historical_data['Close'].iloc[-2]
+        else:
+            prev_close = price  
+        
+        # Extract the latest OHLC (Open, High, Low, Close)
+        latest_ohlc = historical_data[['Open', 'High', 'Low', 'Close']].iloc[-1]
+        
+        # Add to the list of stock data
+        stock_data.append({
+            'Stock Symbol': symbol,
+            'Current Price': price,
+            'Previous Close': prev_close,
+            'Open': latest_ohlc['Open'],
+            'High': latest_ohlc['High'],
+            'Low': latest_ohlc['Low'],
+            'Close': latest_ohlc['Close']
+        })
+        
+    except Exception as e:
+        print(f"Error fetching data for {symbol}: {e}")
+
+# FOR READABILITY
+df = pd.DataFrame(stock_data)
+
+
+if not df.empty:
+    # Sort and display the top 5
+    df_sorted = df.sort_values(by='Current Price', ascending=False).head(5)
+
+    
+    print(tabulate(df_sorted, headers='keys', tablefmt='fancy_grid', showindex=False))
+else:
+    print("No valid data fetched.")
